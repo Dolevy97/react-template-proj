f32924692db1b42d60c57a5ef4e5579784b8bdaa
Dolev Levy <Dolevlevy97@gmail.com>
2024-06-25 13:16:48 +0300

    Test fix github

--- RootCmp.jsx
+++ RootCmp.jsx
@@ -1,18 +1,18 @@
 const Router = ReactRouterDOM.HashRouter
 const { Route, Routes, Navigate } = ReactRouterDOM
 
-import { AppHeader } from "./cmps/AppHeader.jsx"
-import { Home } from "./pages/Home.jsx"
-import { About } from "./pages/About.jsx"
-import { AboutTeam } from "./cmps/AboutTeam.jsx"
-import { AboutGoal } from "./cmps/aboutGoal.jsx"
-import { BookIndex } from "./pages/BookIndex.jsx"
-import { BookDetails } from "./pages/BookDetails.jsx"
-import { BookEdit } from "./pages/BookEdit.jsx"
-import { NotFound } from "./cmps/NotFound.jsx"
-import { UserMsg } from "./cmps/UserMsg.jsx"
-import { AddReview } from "./cmps/AddReview.jsx"
-import { BookAdd } from "./pages/BookAdd.jsx"
+import { AppHeader } from "../cmps/AppHeader.jsx"
+import { Home } from "../pages/Home.jsx"
+import { About } from "../pages/About.jsx"
+import { AboutTeam } from "../cmps/AboutTeam.jsx"
+import { AboutGoal } from "../cmps/aboutGoal.jsx"
+import { BookIndex } from "../pages/BookIndex.jsx"
+import { BookDetails } from "../pages/BookDetails.jsx"
+import { BookEdit } from "../pages/BookEdit.jsx"
+import { NotFound } from "../cmps/NotFound.jsx"
+import { UserMsg } from "../cmps/UserMsg.jsx"
+import { AddReview } from "../cmps/AddReview.jsx"
+import { BookAdd } from "../pages/BookAdd.jsx"
 
 export function App() {
     return (
